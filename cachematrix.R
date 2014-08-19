0de9e0c82614152a96f931bc17f277bd1ad76ce3
Teun Duynstee <github@duynstee.com>
2014-08-19 23:03:19 +0200

    Better encapsulation

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Caching the results of an operation: we have a function that wraps a matrix 
-## and can keep a value for the inverse with it. The second function can operate
-## on this wrapper by checking for a stored inverse and calculating it when 
-## required
+## and can keep a value for the inverse with it. Unlike the example in the readme,
+## I have tried to better encapsulate the functionality inside the same object.
+## The setinv function has been removed and getinv does the caching. I left a
+## dummy implementation of cacheSolve.
 
 ## this is where we create a combination of matrixand its (initially empty) inverse
 makeCacheMatrix <- function(x = matrix()) {
@@ -11,27 +12,16 @@ makeCacheMatrix <- function(x = matrix()) {
     i <<- NULL
   }
   get <- function() x
-  setinv <- function(inv) i <<- inv
-  getinv <- function() i
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
-  
-}
-
-
-## this function does the actual calculation and stores it with the matrix 
-## for later use
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  i <- x$getinv()
-  if(!is.null(i)) {
-    message("using cached data")
-  }else{
-    data <- x$get()
-    ## this is the actual operation that we cache
-    i <- solve(data, ...)
-    x$setinv(i)
+  getinv <- function() {
+    if(!is.null(i)) {
+      message("using cached data")
+    }else{
+      ## this is the actual operation that we cache
+      i <<- solve(x)
+    }
+    i
   }
-  i
+  list(set = set, get = get,
+       getinv = getinv)
 }
+cacheSolve <- function(x){x$getinv()}
